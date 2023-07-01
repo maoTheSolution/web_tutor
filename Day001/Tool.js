f887127cdc633d70cf4aaa0a225c02b0394456d4
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2023-07-02 02:05:02 +0900

    gugudan first update

--- Day001/Tool.js
+++ Day001/Tool.js
@@ -1,5 +1,16 @@
 // console.log("It worked!");
 function gugudan(){
-    var 은지 = document.getElementById("input01").value;
-    document.getElementById("small").textContent = 은지;
+    var result  = "";
+    var dan = document.getElementById("input01").value;
+    for(var i=1; i<10; i++){
+        result = result + dan;
+        result = result + " * ";
+        result = result + i;
+        result = result + " = ";
+        result = result + (dan * i);
+        result = result  + "<br>";
+    }
+
+    // document.getElementById("small").textContent = result;
+    document.getElementById("small").innerHTML = result;
 }   
